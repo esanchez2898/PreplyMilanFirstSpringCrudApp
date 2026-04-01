42edc7f0980331e6581304fa72591b178f344c24
Erick <Erick@Dayanas-MacBook-Pro.local>
2026-03-31 20:55:15 -0600

    t

--- src/main/java/com/erick/spring/service/impl/ProductServiceImpl.java
+++ src/main/java/com/erick/spring/service/impl/ProductServiceImpl.java
@@ -39,8 +39,8 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
-    private final ProductConverter productConverter;
+    private ProductRepository productRepository;
+    private ProductConverter productConverter;
 
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository, ProductConverter productConverter) {
